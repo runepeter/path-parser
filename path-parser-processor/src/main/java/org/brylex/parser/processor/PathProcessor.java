008c4f7f606780ebf19b448ad5a987714beb2e57
Runaldo Rupert Bjørnstad <runepeter@gmail.com>
2026-05-20 08:16:25 +0200

    feat(processor): codegen for String-felt-binding (golden-fil-test)

--- path-parser-processor/src/main/java/org/brylex/parser/processor/PathProcessor.java
+++ path-parser-processor/src/main/java/org/brylex/parser/processor/PathProcessor.java
@@ -5,7 +5,11 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @SupportedAnnotationTypes("org.brylex.parser.annotation.Path")
@@ -14,6 +18,24 @@ public class PathProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        HandlerModelBuilder builder = new HandlerModelBuilder(processingEnv);
+        HandlerCodeGenerator generator = new HandlerCodeGenerator(processingEnv);
+        for (TypeElement annotation : annotations) {
+            Set<TypeElement> handlerTypes = new LinkedHashSet<>();
+            for (Element annotated : roundEnv.getElementsAnnotatedWith(annotation)) {
+                Element enclosing = annotated.getEnclosingElement();
+                if (enclosing instanceof TypeElement type) handlerTypes.add(type);
+            }
+            for (TypeElement type : handlerTypes) {
+                HandlerModel model = builder.build(type);
+                try {
+                    generator.generate(model);
+                } catch (IOException e) {
+                    processingEnv.getMessager().printMessage(
+                            Diagnostic.Kind.ERROR, "Codegen feilet: " + e.getMessage(), type);
+                }
+            }
+        }
         return false;
     }
 }
